89b1da2bb3e9fe5c01ca881a91b3b42c7a9c1bbe
Simeon <srjrsr@abv.bg>
2019-05-05 18:48:36 +0300

    added moving circles

--- game.js
+++ game.js
@@ -1,13 +1,23 @@
 var myX, myY;
+var m_circles = [];
+for (let i = 0; i < 5; ++i){
+    m_circles[i] = new moni04sCircle(Math.random()*700, Math.random()*500, 10, "red");
+}
 function update() {
     myX = mouseX;
     myY = mouseY;
+    for (let i = 0; i < 5; ++i){
+        m_circles[i].move();
+    }
 }
 function draw() {
     context.fillStyle = "blue";
     context.fillRect(100, 200, 100, 200);
     context.fillStyle = "red";
     context.fillRect(myX, myY, 100, 200);
+    for (let i = 0; i < 5; ++i){
+        m_circles[i].draw();
+    }
 };
 function keyup(key) {
     // Show the pressed keycode in the console
