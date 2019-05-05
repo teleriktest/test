827beb085cc468b2b161912fe0025059d5c925f7
Viktor Pazhev <asusvikp@gmail.com>
2019-05-05 17:55:26 +0300

    Enemy removed from game.js

--- game.js
+++ game.js
@@ -1,23 +1,4 @@
 var myX, myY;
-
-function dist(x1, y1, x2, y2) {
-	return Math.sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2));
-}
-
-class Enemy {
-	constructor(x, y, speed) {
-		this.x = x;
-		this.y = y;
-		this.speed = speed;
-	}
-	ai() {
-		var d = dist(this.x, this.y, myX, myY);
-		var dx = (this.x - myX) * this.speed / dist;
-		var dy = (this.y - myY) * this.speed / dist;
-		this.x += dx;
-		this.y += dy;
-	}
-}
 function update() {
     myX = mouseX;
     myY = mouseY;
