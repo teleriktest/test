99aac4c4b0c95f792b5f1dfca1ab409de81b5040
unknown <you@example.com>
2019-05-05 18:42:03 +0300

    rot_rect

--- rot_rect.js
+++ rot_rect.js
@@ -1,12 +1,18 @@
 class RotRect {
   constructor() {
-    this.x = Math.random() * 800;
-    this.y = Math.random() * 600;
+    this.x = 200 + Math.random() * 500;
+    this.y = 100 + Math.random() * 400;
     this.angle =  Math.random() * 2 * Math.PI;
-    this.speed = 5;
   }
   rot() {
-    this.x += Math.cos(this.angle) * speed;
-    this.y += Math.sin(this.angle) * speed;
+    this.angle += 0.05;
+  }
+  draw() {
+    context.translate(this.x, this.y);
+    context.rotate(this.angle);
+    context.fillStyle = "Purple";
+    context.fillRect(-50, -50, 100, 100);
+    context.rotate(-this.angle);
+    context.translate(-this.x, -this.y);
   }
 }
