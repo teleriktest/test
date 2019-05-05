e7e56065c93973feab35921e0dc285ed6ce55815
Oreha <orehaenomer1@gmail.com>
2019-05-05 18:22:28 +0300

    Please enter the commit message for your changes. Lines starting

---
+++ RectangleClass.js
@@ -0,0 +1,40 @@
+﻿// Creating variables
+var myX = 0, myY = 0;
+class OrielRectangle{
+		constructor(){
+			this.x=100;
+			this.y=100;
+			this.speed=5;
+			
+		}
+	update(){
+		if(isKeyPressed[87]){
+			this.y-=this.speed;
+		}
+		if(isKeyPressed[83]){
+			this.y+=this.speed;
+		}
+		if(isKeyPressed[65]){
+			this.x-=this.speed;
+		}
+		if(isKeyPressed[68]){
+			this.x+=this.speed;
+		}
+	}
+	draw(){
+		context.fillRect(this.x,this.y,100,100)
+	}
+	}
+a = new OrielRectangle();
+function update() {
+   a.update();
+}
+
+function draw() {
+   a.draw();
+};
+
+
+
+
+
